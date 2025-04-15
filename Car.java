5e1013b8c60f0669078d29ecda0458362f53bff1
brave29 <147207172+brave29@users.noreply.github.com>
2025-04-15 14:43:28 -0400

    Move car

--- Car.java
+++ Car.java
@@ -4,6 +4,23 @@ public class Car extends Actor
 {
     public void act()
     {
-        move(-3);
+        move(-7);
+        
+        if(getX() <= 0)
+        {
+            resetCar();
+        }
+    }
+    
+    public void resetCar()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0)
+        {
+            setLocation(600,100);
+        } else
+        {
+            setLocation(600, 300);
+        }
     }
 }
